7ff0c8af5aeb01c433ce97a84936a7ba20e206eb
FeGonca <felip.goncalves@hotmail.com>
2023-12-01 15:15:22 -0300

    ghost-gobble-arcade-game

--- ghost-gobble-arcade-game/arcade_game.py
+++ ghost-gobble-arcade-game/arcade_game.py
@@ -8,10 +8,7 @@ def eat_ghost(power_pellet_active, touching_ghost):
     :param touching_ghost: bool - is the player touching a ghost?
     :return: bool - can the ghost be eaten?
     """
-    if power_pellet_active and touching_ghost:
-        return True
-    else:
-        return False
+    return power_pellet_active and touching_ghost
 
 
 def score(touching_power_pellet, touching_dot):
@@ -21,10 +18,7 @@ def score(touching_power_pellet, touching_dot):
     :param touching_dot: bool - is the player touching a dot?
     :return: bool - has the player scored or not?
     """
-    if touching_power_pellet or touching_dot:
-        return True
-    else:
-        return False
+    return touching_power_pellet or touching_dot
 
 
 def lose(power_pellet_active, touching_ghost):
@@ -34,10 +28,8 @@ def lose(power_pellet_active, touching_ghost):
     :param touching_ghost: bool - is the player touching a ghost?
     :return: bool - has the player lost the game?
     """
-    if power_pellet_active == False and touching_ghost == True:
-        return True
-    else:
-        return False
+    return not power_pellet_active and touching_ghost
+
 
 
 def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
@@ -49,10 +41,7 @@ def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
     :return: bool - has the player won the game?
     """
 
-    if touching_ghost and power_pellet_active == False:
+    if touching_ghost and power_pellet_active is False:
         return False
     
-    if has_eaten_all_dots:
-        return True
-    else:
-        return False
+    return has_eaten_all_dots
